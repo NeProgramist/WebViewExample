32ea72760fe6a2b07593fc1aee42ffa42113ff4c
NeProgramist <zasko180@gmail.com>
2020-08-05 15:48:29 +0300

    Initial commit

---
+++ app/src/main/java/com/example/webview/ui/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.webview.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.webview.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
