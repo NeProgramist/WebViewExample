e397b7ba35e433f9c15c707044ac4bb5fca91e8c
NeProgramist <zasko180@gmail.com>
2020-08-06 10:44:13 +0300

    feat(ui): implement splash viewModel [./SplashViewModel.kt]

--- app/src/main/java/com/example/webview/ui/splash/SplashFragment.kt
+++ app/src/main/java/com/example/webview/ui/splash/SplashFragment.kt
@@ -6,9 +6,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 
 import com.example.webview.R
+import kotlinx.android.synthetic.main.splash_fragment.*
 
 class SplashFragment : Fragment() {
     private lateinit var viewModel: SplashViewModel
@@ -23,6 +25,11 @@ class SplashFragment : Fragment() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProvider(this).get(SplashViewModel::class.java)
+
+        viewModel.link.observe(viewLifecycleOwner, Observer {
+            test.text = it
+        })
+        viewModel.getLink()
     }
 
 }
--- app/src/main/java/com/example/webview/ui/splash/SplashViewModel.kt
+++ app/src/main/java/com/example/webview/ui/splash/SplashViewModel.kt
@@ -1,7 +1,31 @@
 package com.example.webview.ui.splash
 
 import android.app.Application
+import android.util.Log
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.MutableLiveData
+import com.example.webview.domain.interactor.GetLinkUseCase
+import com.example.webview.domain.repository.LinksRepositoryImpl
+import com.example.webview.framework.datasource.local.LocalLinkDataSource
+import com.example.webview.framework.datasource.remote.RemoteLinkDataSource
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
+import io.reactivex.rxjava3.schedulers.Schedulers
 
 class SplashViewModel(application: Application) : AndroidViewModel(application) {
+    val link = MutableLiveData<String>()
+
+    fun getLink() {
+        GetLinkUseCase(
+            LinksRepositoryImpl(
+                LocalLinkDataSource(getApplication()),
+                RemoteLinkDataSource()
+            )
+        )()
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe (
+                { link.value = it; Log.e("asdfasdf", "here") },
+                { Log.e("asdfasdf", "An error occurred - $it") }
+            )
+    }
 }
